63c7724330848ddee575832384be17442e54e3e6
Viktor Kindrat <84918090+viktor-kindrat@users.noreply.github.com>
2025-09-14 11:45:50 +0300

    add metadata

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,5 @@
 import LoginForm from '../../../components/auth/login-form';
 
-export const dynamic = 'force-dynamic';
-
 export default function LoginPage({ searchParams }: { searchParams?: { error?: string } }) {
   const error = searchParams?.error;
   return <LoginForm formError={error} />;
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,7 +1,6 @@
 import RegisterForm from '../../../components/auth/register-form';
 
-export const dynamic = 'force-dynamic';
-
 export default function RegisterPage() {
   return <RegisterForm />;
 }
+
